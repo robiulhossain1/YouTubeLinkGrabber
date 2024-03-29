c8f4a762b4b0f2345ae18674a2a94ea4de03ef69
Robiul Hossain <78794958+robiulhossain1@users.noreply.github.com>
2024-03-29 20:40:20 +0600

    Update YouTubeLinkGrabber.py

--- YouTubeLinkGrabber.py
+++ YouTubeLinkGrabber.py
@@ -2,6 +2,10 @@
 import requests
 import os
 
+windows = False
+if 'win' in sys.platform:
+    windows = True
+
 def grab(url):
     response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
@@ -19,8 +23,8 @@ def grab(url):
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
-        if 'https://' in response[end - tuner : end]:
-            link = response[end - tuner : end]
+        if 'https://' in response[end-tuner : end]:
+            link = response[end-tuner : end]
             start = link.find('https://')
             end = link.find('.m3u8') + 5
             break
