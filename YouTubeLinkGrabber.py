c22aa108db7a3ac6e1cd175033a3d671c234e94b
Robiul Hossain <78794958+robiulhossain1@users.noreply.github.com>
2024-03-29 20:37:29 +0600

    Update YouTubeLinkGrabber.py

--- YouTubeLinkGrabber.py
+++ YouTubeLinkGrabber.py
@@ -3,10 +3,19 @@ import requests
 import os
 
 def grab(url):
-    response = requests.get(url, timeout = 15).text
+    response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
-        print("https://www.youtube.com/watch?v=1oh9IEwBbFY")
-        return
+        #response = requests.get(url).text
+        if '.m3u8' not in response:
+            if windows:
+                print('https://arafatplay.top/Video/livestream.m3u8')
+                return
+            #os.system(f'wget {url} -O temp.txt')
+            os.system(f'curl "{url}" > temp.txt')
+            response = ''.join(open('temp.txt').readlines())
+            if '.m3u8' not in response:
+                print('https://arafatplay.top/Video/livestream.m3u8')
+                return
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
